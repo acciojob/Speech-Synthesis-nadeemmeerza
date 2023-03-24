37e3e0a1af0b842428345359f56b162c1451c73d
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-03-24 14:25:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Your script here.
+let speak = getElementById("speak")
+let stop = getElementById("stop")
+
+speak.addEventListener("click", (e)=>{
+	let text = getElementById("text").value
+	let utterance = new speechSynthesisUtterance();
+	utterance.text = text;
+	text.value = "hi i am nadeem"
+	speechSynthesis.speak(utterance);
+})
